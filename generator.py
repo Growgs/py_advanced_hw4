72e8ff2fb44dab5c02f49af709ea65da46024d54
Growgs <yazikov@yandex.ru>
2023-02-19 22:31:05 +0300

    initial commit

---
+++ generator.py
@@ -0,0 +1,59 @@
+import types
+
+num_list = [[1,[33, 5],2,3], None, [1,2]]
+
+def flat_generator(my_list):
+    for element in my_list:
+        if not isinstance(element, list):
+            yield element
+        else:
+            for item in flat_generator(element):
+                yield item
+
+for i in flat_generator(num_list):
+    print(i)
+
+def test_2():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+def test_4():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_2()
+    test_4()
+
+
---
+++ iterator.py
@@ -0,0 +1,63 @@
+
+nested_list = [[1, 'b'], [3, [4, 5]], None, [7, True]]
+
+class FlatIterator:
+    def __init__(self, my_list):
+        self.my_list = my_list[::-1]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.my_list:
+            item = self.my_list.pop()
+            if isinstance(item, list):
+                self.my_list.extend(item[::-1])
+            else:
+                return item
+        raise StopIteration
+
+
+flat_list = FlatIterator(nested_list)
+for elem in flat_list:
+    print(elem)
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+def test_3():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+
+if __name__ == '__main__':
+    test_1()
+    test_3()
